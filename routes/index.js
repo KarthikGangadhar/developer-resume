3128cb15d29e7c3e4c68f8dfce05a7d031404220
karthik <karthikg1643@gmail.com>
2017-11-28 22:19:12 +0530

    added generalized profile config.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var profile = require("../config/profile")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { profile: profile });
 });
 
 module.exports = router;
