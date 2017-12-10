412d09744a0d879af29968667c0b887d749b3fc5
karthik <karthikg1643@gmail.com>
2017-12-10 22:39:29 +0530

    added stackoverflow section lib files

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 const profile = require("../config/profile")
 const settings = require("../config/settings")
+const soPosts = require("../lib/so_posts")
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { profile: profile, settings: settings });
+soPosts.getPosts().then((resp) => {
+  router.get('/', function (req, res, next) {
+    res.render('index', {
+      profile: profile,
+      settings: settings,
+      stPost: resp
+    });
+  });
 });
+
 
 module.exports = router;
